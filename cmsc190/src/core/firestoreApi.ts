379de414297a65db4494e608174acf5197f7e01a
Jaimy Arcilla <jdarcilla@up.edu.ph>
2023-09-20 00:16:11 +0800

    ✨ Use firebase emulators; Initial commit for cloud functions

--- cmsc190/src/core/firestoreApi.ts
+++ cmsc190/src/core/firestoreApi.ts
@@ -1,31 +1,28 @@
-import firestore, {
-  FirebaseFirestoreTypes,
-} from '@react-native-firebase/firestore';
+import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
+import { fs } from './fb';
 import { undef2null } from './utils';
 
 export type Cursor =
   FirebaseFirestoreTypes.QueryDocumentSnapshot<FirebaseFirestoreTypes.DocumentData>;
 
 const getDoc = async <T>(path: string): Promise<T | null> => {
-  const doc = await firestore().doc(path).get();
+  const doc = await fs.doc(path).get();
   if (doc.exists) return doc.data() as T;
   return null;
 };
 
 const putDoc = async <T>(path: string, obj: T): Promise<void> => {
   undef2null(obj);
-  return firestore()
-    .doc(path)
-    .set(obj as { [x: string]: any });
+  return fs.doc(path).set(obj as { [x: string]: any });
 };
 
 const mergeDoc = async <T>(path: string, obj: Partial<T>): Promise<void> => {
   undef2null(obj); // firestore does not support undefined as a value
-  return firestore().doc(path).set(obj, { merge: true });
+  return fs.doc(path).set(obj, { merge: true });
 };
 
 const deleteDoc = async (path: string): Promise<void> => {
-  return firestore().doc(path).delete();
+  return fs.doc(path).delete();
 };
 
 const buildCollectionQuery = <T>(args: {
@@ -49,7 +46,7 @@ const buildCollectionQuery = <T>(args: {
   } = args;
 
   // create query and where clause
-  let query = firestore().collection(path).where(where[0], where[1], where[2]);
+  let query = fs.collection(path).where(where[0], where[1], where[2]);
   // where2 clause
   if (where2) query = query.where(where2[0], where2[1], where2[2]);
   // order by
